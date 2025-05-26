875a3153da2c514bda9568035ea780ef3ca60448
0victorsales <victorhsales0@gmail.com>
2025-05-25 22:32:57 -0300

    change: alterado arquivo config

--- app/config.py
+++ app/config.py
@@ -2,11 +2,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH = os.path.join(BASE_DIR, "banco", "database.db")
 
 class Configuracoes:
     PROJETO: str = "beber_agua"
-    SQLALCHEMY_DATABASE_URL: str = "sqlite:///C:/Users/SAMSUNG/Desktop/parceiro/PYTHON/backend-beber_agua/app/banco/database.db"
-
+    SQLALCHEMY_DATABASE_URL: str = f"sqlite:///{DB_PATH}"
 
 configuracoes = Configuracoes()
 
